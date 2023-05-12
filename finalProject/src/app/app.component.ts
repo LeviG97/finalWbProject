b1f7825096bcfca751643826afd524ffd18ce358
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-05-12 16:34:38 +0100

    Commit subscribing to observable.

--- finalProject/src/app/app.component.ts
+++ finalProject/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IOMDBResponse } from './bookResponse';
+import { BookApiService } from './services/book-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finalProject';
+  movieData:IOMDBResponse | undefined;
+  errorMessage:any;
+
+  constructor(private _omdbService:BookApiService){}
+
+  getMovieDetails(movieName:string): boolean{
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director name : " + this.movieData.Director);
+      }
+    )
+    return false;
+  }
+  
 }
--- finalProject/src/app/app.module.ts
+++ finalProject/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {BookApiService} from './services/book-api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
