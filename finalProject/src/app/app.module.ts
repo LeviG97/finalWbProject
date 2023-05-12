74310939dff8bb2a88fbd663259e79b873f75532
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-05-12 21:37:50 +0100

    Commit that added nav and solved nav issue. There was missing RouterModule.forRoot(routes) in imports

--- finalProject/src/app/app.module.ts
+++ finalProject/src/app/app.module.ts
@@ -4,17 +4,34 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {BookApiService} from './services/book-api.service';
 import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes=[
+  {path:'', component:SearchtitleComponent},
+  {path:'about', component:AboutComponent},
+  {path: 'search', component:SearchComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchtitleComponent
+    SearchtitleComponent,
+    AboutComponent,
+    SearchComponent
+    
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [BookApiService],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
