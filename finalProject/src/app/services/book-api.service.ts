17c93055ecd6fa67a1202aa4356c5995b98f3b50
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-05-12 16:14:49 +0100

    Commit setting up book service to get api.

---
+++ finalProject/src/app/services/book-api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs';
+import { IOMDBResponse } from '../bookResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookApiService {
+
+  private _siteURL="https://www.omdapi.com"
+  private _key="?i=tt3896198&apikey=b6a4a20a"
+  constructor(private _http:HttpClient) { }
+
+  getMovieData(movieName:string):Observable<IOMDBResponse>{
+    return this._http.get<IOMDBResponse>(this._siteURL + this._key + movieName)
+    .pipe(
+      tap(data=> console.log('Moviedata/error' +JSON.stringify(data)
+      )),
+      catchError(this.handleError)
+    );
+  }
+}
