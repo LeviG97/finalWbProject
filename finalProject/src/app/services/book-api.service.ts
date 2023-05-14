35d05a1865c17719260c46096c379b90135c4fc8
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-05-14 16:03:40 +0100

    Commit set up search with multiple returns.

--- finalProject/src/app/services/book-api.service.ts
+++ finalProject/src/app/services/book-api.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs';
 import { IOMDBResponse } from '../bookResponse';
+import { IOMDBResponse2 } from '../bookResponse2';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class BookApiService {
 
   private _siteURL="https://www.omdbapi.com/"
   private _key="?apikey=b6a4a20a&t="
+  private _key2="?apikey=b6a4a20a&s="
   constructor(private _http:HttpClient) { }
 
   getMovieData(movieName:string):Observable<IOMDBResponse>{
@@ -24,5 +26,14 @@ export class BookApiService {
   private handleError(err:HttpErrorResponse){
     console.log('OmdbApiService:' + err.message);
     return throwError(() => new Error("OmdbApiService:" + err.message))
+  }
+
+  getMoviesData(movieName:string, page:number):Observable<IOMDBResponse2> {
+    return this._http.get<IOMDBResponse2>(this._siteURL+ this._key2 + movieName + "&page=" + page)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
   }
 }
